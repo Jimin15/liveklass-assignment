0170d0ef07a341e8040569a1353c9c177038cc19
Jimin15 <sujean07015@gmail.com>
2026-05-18 03:12:02 +0900

    chore: Spring Boot 프로젝트 초기 생성

---
+++ src/main/java/com/example/liveklass/LiveklassApplication.java
@@ -0,0 +1,13 @@
+package com.example.liveklass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class LiveklassApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LiveklassApplication.class, args);
+    }
+}
